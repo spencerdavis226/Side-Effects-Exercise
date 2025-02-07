5b3f277c422e6fa9c52892bfea9db18967d8c1dc
Spencer Davis <spencerdavis226@gmail.com>
2025-02-07 16:55:06 -0500

    Add useRef and useEffect to focus Star component on mount

--- src/Star.jsx
+++ src/Star.jsx
@@ -1,5 +1,21 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
-const Star = () => {};
+const Star = ({ id, position, onDestroy }) => {
+  // Create a ref for the star element
+  const starRef = useRef(null);
+
+  // Focus the star when it mounts
+  useEffect(() => {
+    if (starRef.current) {
+      starRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <div ref={starRef} tabIndex={0}>
+      {/* Star content will go here */}
+    </div>
+  );
+};
 
 export default Star;
